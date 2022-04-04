da29ca0b9a043ccf692972a087191a1ea5ac92df
Davide Depau <davide@depau.eu>
2022-04-04 23:19:32 +0200

    Update all desktop icons

--- appimage/make_appdir.sh
+++ appimage/make_appdir.sh
@@ -1,16 +1,35 @@
 #!/bin/bash
 
+# Clear old AppDir
 rm -rf AppDir || true
+
+# Copy build to AppDir
 cp -r atcd_choreo_sync/build/linux/x64/release/bundle AppDir
+
+# Set up desktop files
 mkdir -p AppDir/usr/share/applications
-cp atcd_choreo_sync/android/app/src/main/res/mipmap-xxxhdpi/ic_launcher.png AppDir/atcd_choreo_sync.png
 cp appimage/atcd_choreo_sync.desktop AppDir/gay.depau.atcd_choreo_sync.desktop
 cp appimage/atcd_choreo_sync.desktop AppDir/usr/share/applications/gay.depau.atcd_choreo_sync.desktop
-mkdir -p AppDir/usr/share/icons/hicolor/192x192/apps/
-mkdir -p AppDir/usr/share/icons/hicolor/96x96/apps/
-mkdir -p AppDir/usr/share/icons/hicolor/48x48/apps/
-cp atcd_choreo_sync/android/app/src/main/res/mipmap-xxxhdpi/ic_launcher.png AppDir/usr/share/icons/hicolor/192x192/apps/atcd_choreo_sync.png
-cp atcd_choreo_sync/android/app/src/main/res/mipmap-xhdpi/ic_launcher.png AppDir/usr/share/icons/hicolor/96x96/apps/atcd_choreo_sync.png
-cp atcd_choreo_sync/android/app/src/main/res/mipmap-mdpi/ic_launcher.png AppDir/usr/share/icons/hicolor/48x48/apps/atcd_choreo_sync.png
+
+# Set up main icon
+cp atcd_choreo_sync/macos/Runner/Assets.xcassets/AppIcon.appiconset/app_icon_512.png AppDir/atcd_choreo_sync.png
+
+# Set up hicolor icon theme icons
+mkdir -p AppDir/usr/share/icons/hicolor/1024x1024/apps/
+cp atcd_choreo_sync/macos/Runner/Assets.xcassets/AppIcon.appiconset/app_icon_1024.png AppDir/usr/share/icons/hicolor/1024x1024/apps/
+mkdir -p AppDir/usr/share/icons/hicolor/128x128/apps/
+cp atcd_choreo_sync/macos/Runner/Assets.xcassets/AppIcon.appiconset/app_icon_128.png AppDir/usr/share/icons/hicolor/128x128/apps/
+mkdir -p AppDir/usr/share/icons/hicolor/16x16/apps/
+cp atcd_choreo_sync/macos/Runner/Assets.xcassets/AppIcon.appiconset/app_icon_16.png AppDir/usr/share/icons/hicolor/16x16/apps/
+mkdir -p AppDir/usr/share/icons/hicolor/256x256/apps/
+cp atcd_choreo_sync/macos/Runner/Assets.xcassets/AppIcon.appiconset/app_icon_256.png AppDir/usr/share/icons/hicolor/256x256/apps/
+mkdir -p AppDir/usr/share/icons/hicolor/32x32/apps/
+cp atcd_choreo_sync/macos/Runner/Assets.xcassets/AppIcon.appiconset/app_icon_32.png AppDir/usr/share/icons/hicolor/32x32/apps/
+mkdir -p AppDir/usr/share/icons/hicolor/512x512/apps/
+cp atcd_choreo_sync/macos/Runner/Assets.xcassets/AppIcon.appiconset/app_icon_512.png AppDir/usr/share/icons/hicolor/512x512/apps/
+mkdir -p AppDir/usr/share/icons/hicolor/64x64/apps/
+cp atcd_choreo_sync/macos/Runner/Assets.xcassets/AppIcon.appiconset/app_icon_64.png AppDir/usr/share/icons/hicolor/64x64/apps/
+
+# Swap AppRun binary with custom script
 cp appimage/AppRun AppDir/AppRun
 chmod +x AppDir/AppRun
